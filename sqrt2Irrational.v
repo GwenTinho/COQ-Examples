5b0f7f347595e5f737f971fbe927336cac5decdb
Gwentinho <qschroed@gmail.com>
2023-01-20 18:27:08 +0100

    lol

--- sqrt2Irrational.v
+++ sqrt2Irrational.v
@@ -44,11 +44,52 @@ Proof.
   apply H.
 Qed.
 
+Lemma next_even_is_odd : forall x : nat,
+  Nat.even x = negb (Nat.even (S x)).
+Proof.
+  intros.
+  induction x.
+  - simpl.
+    reflexivity.
+  - simpl.
+    rewrite -> IHx.
+    rewrite -> Bool.negb_involutive.
+    reflexivity.
+Qed.
+
+Lemma cancel_negb : forall a b: bool,
+  negb a = negb b -> a = b.
+Proof.
+  intros.
+  destruct a.
+  - destruct b.
+    + reflexivity.
+    + simpl in H.
+      symmetry.
+      apply H.
+  - destruct b.
+    + simpl in H.
+      symmetry.
+      apply H.
+    + reflexivity.
+Qed.
+
 Lemma even_is_xor: forall x y : nat,
   Nat.even (x+y) = negb (xorb (Nat.even x) (Nat.even y)).
 Proof.
-Admitted.
-
+  intros.
+  induction x.
+  - simpl.
+    case (Nat.even y); reflexivity.
+  - rewrite next_even_is_odd in IHx.
+    rewrite (next_even_is_odd x) in IHx.
+    apply cancel_negb in IHx.
+    rewrite Nat.add_succ_l.
+    rewrite -> IHx.
+    clear IHx.
+    rewrite Bool.negb_xorb_l.
+    reflexivity.
+Qed.
 
 Lemma square_even_is_even : forall x : nat,
   Nat.even (x*x) = Nat.even x.
@@ -91,19 +132,21 @@ Proof.
 Qed.
 
 
-
-
-
-
-
-
-
-Theorem sqrt2_is_irrational: forall p q: nat,
-  p*p <> 2*q*q.
-
+Lemma cancel_2: forall x y : nat,
+  2 * x = 2 * y -> x = y.
 Proof.
   intros.
-  intros eq.
+  lia.
+Qed.
+
+
+Theorem sqrt2_infinite_descent: forall p q,
+ q <> 0 -> p*p = 2*q*q -> exists pp qq : nat,
+ pp * pp = 2 * qq * qq /\ qq < q.
+Proof.
+  intros p q qnz eq.
+  assert (eq' := eq).
+
   assert (Nat.even (p*p) = true).
   {
     rewrite -> eq.
@@ -120,6 +163,82 @@ Proof.
   apply even_exists in H0.
   destruct H0 as [p' Hp'].
   rewrite <- Hp' in eq.
+  repeat rewrite <- Nat.mul_assoc in eq.
+  apply cancel_2 in eq.
+  rewrite Nat.mul_comm in eq.
+  clear H.
 
+  assert (Nat.even (q*q) = true).
+  {
+    rewrite <- eq.
+    rewrite <- Nat.mul_assoc.
+    apply x_times_2_is_even.
+  }
+
+  assert (Nat.even q = true).
+  {
+    rewrite square_even_is_even in H.
+    apply H.
+  }
+
+  apply even_exists in H0.
+  destruct H0 as [q' Hq'].
+  rewrite <- Hq' in eq.
+  repeat rewrite <- Nat.mul_assoc in eq.
+  apply cancel_2 in eq.
+  symmetry in eq.
+  rewrite Nat.mul_comm in eq.
+  clear H.
+  symmetry in eq.
+
+  assert (q > q').
+  lia.
+  clear Hp' Hq'.
+
+  exists p'.
+  exists q'.
+
+  split.
+  - apply eq.
+  - apply H.
+Qed.
+
+Definition lt_nat (p q : nat*nat) := snd p < snd q.
+
+Theorem lt_wf: well_founded lt_nat.
+Proof.
+  unfold well_founded.
+  intros.
+
+Admitted.
+
+Theorem infinite_descent: forall P : Prop -> forall p q : nat,
+  (P p q) -> exists p' q' : nat, (f p' q') /\ q' < q ->.
+Proof.
+
+Qed.
+
+
+Theorem sqrt2_is_irrational: forall p q: nat,
+  q <> 0 -> p*p <> 2*q*q.
+Proof.
+  intros p q qnz.
+  unfold not.
+  intros eq.
+  generalize sqrt2_infinite_descent.
+  intros.
+  specialize (H p q).
+  apply H in qnz.
+  destruct qnz.
+  destruct H0.
+  destruct H0.
+  2 : {
+    apply eq.
+  }
+  clear H.
+
+
+
+Qed.
 
   Set Printing All
